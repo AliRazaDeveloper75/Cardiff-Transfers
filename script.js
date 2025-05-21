b654ea1c3fca72cdf38074e08dc5ebaf6d014ed1
AliRazaDeveloper75 <alirazadeveloper75@gmail.com>
2025-05-21 10:25:11 +0500

    first commiadft

---
+++ script.js
@@ -0,0 +1,32 @@
+// Load header and footer
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("header.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("header-placeholder").innerHTML = data;
+      initScrollEffect();
+    });
+
+  fetch("footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer-placeholder").innerHTML = data;
+    });
+});
+
+// Sticky header scroll effect
+function initScrollEffect() {
+  const header = document.querySelector('.site-header');
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+}
+
+
+ function toggleMenu() {
+    document.getElementById("mainNav").classList.toggle("show");
+  }
